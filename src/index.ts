c7c0b43eff221be8049bc3fc25b5ab135ecd65ed
santimenendez98 <santi.mm11@gmail.com>
2025-07-10 20:26:41 -0300

    Setted postgresql

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import pool from "./db";
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("Welcome to the Matchplay Server!");
-});
-
-app.listen(() => {
+app.listen(Number(process.env.PORT), () => {
+  pool
+    .connect()
+    .then(() => console.log("Connected to the database successfully"));
   console.log(`Server is running on ${process.env.API_URL}${process.env.PORT}`);
 });
